bf2e6f6d113e747bd9b90a0e61e5b9934d745715
JRamosEEC <jdramos1@email.neit.edu>
2024-05-16 07:05:19 -0400

    Dap Keybind Updates & Sync All Changes To Branch

--- init.lua
+++ init.lua
@@ -504,9 +504,11 @@ require("nvim-dap-virtual-text").setup({
   virt_line = false,
   virt_text_win_col = nil,
 })
--- Dap Keybinds
+-- Dap Keybinds - (looin into dap.up() & dap.down() and figure out good keybings for quick stck traversal)
+vim.keymap.set('n', '<F2>', function() require('dap').step_over() end)
+vim.keymap.set('n', '<F3>', function() require('dap').step_into() end)
+vim.keymap.set('n', '<F4>', function() require('dap').step_out() end)
 vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
-vim.keymap.set('n', '<F9>', function() require('dap').toggle_breakpoint() end)
-vim.keymap.set('n', '<F10>', function() require('dap').step_over() end)
-vim.keymap.set('n', '<F11>', function() require('dap').step_into() end)
-vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)
+vim.keymap.set('n', '<F6>', function() require('dap').close() end)
+vim.keymap.set('n', '<F9>', function() require('dap').run_to_cursor() end)
+vim.keymap.set('n', '<F10>', function() require('dap').toggle_breakpoint() end)
---
+++ rust/grep-history/notes.md
@@ -0,0 +1,7 @@
+TODO:
+This will be the start of the rust app to manage search history and grep parsing when previewed.
+I'm going to experiment to see if it's faster to RPC the data including parsing on preview.
+This might provide bonusses where I can bring back the previewer perks like icons.
+I'm thinking maybe like a batched parser on preview that does the visible entries and a page a head and loads the icons like we used to have (If it's possible to "re-render" picker)
+
+Resources: Take a look at medium.com - article a-detailed-guide-to-writing-your-first-neovim-plugin-in-rust
